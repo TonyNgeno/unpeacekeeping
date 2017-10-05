d4feb27393812965ebc40152fccb211c5cfca377
Brian Okinyi <brianokinyi.bo@gmail.com>
2017-10-05 20:18:12 +0300

    Interlinked all pages

--- register.php
+++ register.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <meta name="description" content="">
+    <meta name="description" content="Register to be a peace ambassador">
     <meta name="author" content="Emmanuel Gathage & Brian Okinyi">
     <meta name="keywords" content="un, United Nations, UN Peace Keeping, peace, Kenya, register">
     <meta name="twitter:description" content="UN Peace Keeping Ke - Register">
@@ -51,57 +51,51 @@
             <div class="container">
                 <div class="row">
                     <div class="col-sm-6">
-                        <p>UN PeaceKeeping KE</p>
                     </div>
                     <div class="col-sm-6">
                         <ul class="list-inline pull-right">
-                            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                            <li><a href="#"><i class="fa fa-envelope-o"></i></a></li>
-                            <li><a href="tel:+254742796000"><i class="fa fa-phone"></i> 0742796000</a></li>
+                            <li><a href="#" title="Post"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="#" title="Post"><i class="fa fa-twitter"></i></a></li>
+                            <li><a href="#" title="Post"><i class="fa fa-linkedin"></i></a></li>
+                            <li><a href="#" title="Post"><i class="fa fa-envelope-o"></i></a></li>
+                            <li><a href="tel:+254742796000"><i class="fa fa-phone"></i>0742796000</a></li>
                         </ul>                        
                     </div>
                 </div>
             </div>
         </div>
         <nav class="navbar navbar-default">
-			<div class="container">
-				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse">
-					<span class="sr-only">Toggle Navigation</span>
-					<i class="fa fa-bars"></i>
-				</button>
-				<a href="index.html" class="navbar-brand">
-					<img src="../img/un_logo.png" alt="Post">
-				</a>
+            <div class="container">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a href="index.html" class="navbar-brand">
+                    <img src="img/un_logo.png" alt="Post">
+                </a>
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-navbar-collapse">
                     <ul class="nav navbar-nav main-navbar-nav">
                         <li><a href="index.html" title="">HOME</a></li>
-                        <li class="dropdown">
-                            <a href="#" title="" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">DROPDOWN MENU <span class="caret"></span></a>
-                            <ul class="dropdown-menu">
-                                <li><a href="#" title="">SUB MENU 1</a></li>
-                                <li><a href="#" title="">SUB MENU 2</a></li>
-                                <li><a href="#" title="">SUB MENU 3</a></li>
-                            </ul>
-                        </li>
-                        <li class="active"><a href="page.html" title="">PAGE</a></li>
-                        <li><a href="category.html" title="">CATEGORY</a></li>
-                        <li><a href="#" title="">MENU ITEM</a></li>
-                        <li><a href="#" title="">MENU ITEM</a></li>
-                    </ul>                           
+                        <li><a href="category.html" title="">NEWS</a></li>
+                        <li><a href="index.html#services" title="">OUR MISSION</a></li>
+                        <li><a href="#" title=""></a></li>
+                        <li><a href="#" title=""></a></li>
+                    </ul>  
+                    <ul class="nav navbar-nav main-navbar-nav navbar-right">
+                      <li><a href="index.html#apply"><span class="glyphicon glyphicon-user"></span>&nbsp;Become an ambassador</a></li>
+                    </ul>                         
                 </div><!-- /.navbar-collapse -->                
-				<!-- END MAIN NAVIGATION -->
-			</div>
-		</nav>        
+                <!-- END MAIN NAVIGATION -->
+            </div>
+        </nav>        
     </header>
     <div class="bread_area">
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
                     <ol class="breadcrumb">
-                        <li><a href="../index.html">Home</a></li>
+                        <li><a href="index.html" title="Home">Home</a></li>
                         <li class="active">Member registration</li>
                     </ol>                    
                 </div>
@@ -250,54 +244,6 @@
                                   </div>
                                 </div>
 
-                                <!-- KCSE year of exams
-                                <div class="col-half">
-                                    <h5>KCSE Year of Exams</h5>
-                                    <div class="">
-                                    <div class="">
-                                        <select>
-                                            <option value="2016">2016</option>
-                                            <option value="2015">2015</option>
-                                            <option value="2014">2014</option>
-                                            <option value="2013">2013</option>
-                                            <option value="2012">2012</option>
-                                            <option value="2011">2011</option>
-                                            <option value="2010">2010</option>
-                                            <option value="2009">2009</option>
-                                            <option value="2008">2008</option>
-                                            <option value="2007">2007</option>
-                                            <option value="2006">2006</option>
-                                            <option value="2005">2005</option>
-                                            <option value="2004">2004</option>
-                                            <option value="2003">2003</option>
-                                            <option value="2002">2002</option>
-                                            <option value="2001">2001</option>
-                                            <option value="2000">2000</option>
-                                            <option value="1999">1999</option>
-                                            <option value="1998">1998</option>
-                                            <option value="1997">1997</option>
-                                            <option value="1996">1996</option>
-                                            <option value="1995">1995</option>
-                                            <option value="1994">1994</option>
-                                            <option value="1993">1993</option>
-                                            <option value="1992">1992</option>
-                                            <option value="1991">1991</option>
-                                            <option value="1990">1990</option>
-                                            <option value="1989">1989</option>
-                                            <option value="1988">1988</option>
-                                            <option value="1987">1987</option>
-                                            <option value="1986">1986</option>
-                                            <option value="1985">1985</option>
-                                            <option value="1984">1984</option>
-                                            <option value="1983">1983</option>
-                                            <option value="1982">1982</option>
-                                            <option value="1981">1981</option>
-                                            <option value="1980">1980</option>
-                                        </select>
-                                    </div>
-                                    </div>
-                            </div>
-                            //Kcse year-->
                                 <h5>Brief explanation about your education background</h5>
                                 <div class="input-group input-group-icon" style="width: 80%;">
                                     <textarea name="education_desc" cols="" rows="5" placeholder="Short about your education"></textarea>
@@ -337,59 +283,48 @@
                     <div class="widget">
                         <h4>SERVICES</h4>
                         <ul>
-                            <li class="current"><a href="#" title="">Service Title One</a></li>
-                            <li><a href="#" title="">Service Title Two</a></li>
-                            <li><a href="#" title="">Service Title Three</a></li>
-                            <li><a href="#" title="">Service Title Four</a></li>
+                            <li class="current"><a href="#" title="">Register</a></li>
+                            <li><a href="category.html" title="">News</a></li>
                         </ul>
+                    </div>
                     </div>
                 </aside>
             </div>
         </div>
     </main>
-    <footer class="site-footer">
+   <footer class="site-footer">
         <div class="container">
             <div class="row">
-                <div class="col-md-3 col-sm-6 fbox">
-                    <h4>UN PeaceKeeping KE</h4>
-                    <p class="text"></p>
+                <div class="col-md-3 col-sm-6 col-xs-12 fbox">
+                    <h4>UNPEACEKEEPINGKE</h4>
+                    <p class="text">The primary, the fundamental, the essentila purpose of the UNPEACEKEEPINGKE is to keep peace.</p>
                     <ul class="list-inline">
-                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                        <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                        <li><a href="#"><i class="fa fa-linkedin"></i></a></li>                        
+                        <li><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                        <li><a href="#" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                        <li><a href="#" title="Linked in"><i class="fa fa-linkedin"></i></a></li>
+                        <li><a href="#" title="Google +"><i class="fa fa-google-plus"></i></a></li>                       
                     </ul>
                 </div>
-                <div class="col-md-3 col-sm-6 fbox">
-                    <h4>SERVICES</h4>
+                <div class="col-md-3 col-sm-6 col-xs-12 fbox">
+                    <h4>ORGANISATION</h4>
                     <ul class="big">
-                        <li><a href="#" title="">Title One</a></li>
-                        <li><a href="#" title="">Title Two</a></li>
-                        <li><a href="#" title="">Title Three</a></li>
-                        <li><a href="#" title="">Title Four</a></li>
-                        <li><a href="#" title="">Title Five</a></li>
-                        <li><a href="#" title="">Title Six</a></li>
-                        <li><a href="#" title="">Title Seven</a></li>
-                        <li><a href="#" title="">Title Eight</a></li>
+                        <li><a href="#" title="Register">Register</a></li>
+                        <li><a href="#" title="">&nbsp;</a></li>
+                        <li><a href="category.html" title="News">News</a></li>
                     </ul>
                 </div>
-                <div class="col-md-3 col-sm-6 fbox">
-                    <h4>CONTENT</h4>
+                <div class="col-md-3 col-sm-6 col-xs-12 fbox">
+                    <h4>UPDATES</h4>
                     <ul class="big">
-                        <li><a href="#" title="">News</a></li>
-                        <li><a href="#" title="">Title Two</a></li>
-                        <li><a href="#" title="">Title Three</a></li>
-                        <li><a href="#" title="">Title Four</a></li>
-                        <li><a href="#" title="">Title Five</a></li>
-                        <li><a href="#" title="">Title Six</a></li>
-                        <li><a href="#" title="">Title Seven</a></li>
-                        <li><a href="#" title="">Title Eight</a></li>
+                        <li><a href="index.html#login-form" title="">Subscribe</a></li>
+                        <li><a href="#" title="">&nbsp;</a></li>
                     </ul>
                 </div>
-                <div class="col-md-3 col-sm-6 fbox">
-                    <h4>CONTENT</h4>
-                    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                    <p><a href="tel:+902222222222"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span> +90 222 222 22 22</a></p>
-                    <p><a href="mailto:iletisim@agrisosgb.com"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> mail@awebsitename.com</a></p>
+                <div class="col-md-3 col-sm-6 col-xs-12 fbox">
+                    <h4>CONTACT US</h4>
+                    <p class="text">UNPEACEKEEPINGKE</p>
+                    <p><a href="tel:+254742796000"><span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>0742796000</a></p>
+                    <p><a href="mailto:iletisim@agrisosgb.com"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span>unpeacekeepingke@gmail.com</a></p>
                 </div>
             </div>
         </div>
@@ -397,16 +332,13 @@
             <div class="container">
                 <div class="row">
                     <div class="col-md-4">
-                        <p class="pull-left">&copy; 2016 COMPANY NAME</p>
+                        <a href="#" title="UN PeaceKeepingKE"><p class="pull-left">&copy; 2017 UN PeaceKeepingKE</p></a>
                     </div>
                     <div class="col-md-8">
                         <ul class="list-inline navbar-right">
-                            <li><a href="#">HOME</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
-                            <li><a href="#">MENU ITEM</a></li>
+                            <li><a href="index.html" title="Home">HOME</a></li>
+                            <li><a href="category.html">NEWS</a></li>
+                            <li><a href="index.html#services" title="Our Mission">OUR MISSION</a></li>
                         </ul>
                     </div>
                 </div>
